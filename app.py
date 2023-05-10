8e2937ec136688a182a5a7928cabef67457d695b
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2023-05-10 22:38:46 +0900

    flask rest api

--- app.py
+++ app.py
@@ -1,22 +1,32 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from face_recognition.face_recognition import face_recognition
 from yolov5.run_yolov5 import run_yolov5
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return "test"
 
 
-@app.route('/face')
-def face(images):
-    return face_recognition(images)
+@app.route('/face', methods=['POST'])
+def face():
+    payload = request.get_json()
+    result = face_recognition(payload['image_list'])
+    response = {
+        'group': result[0],
+        'group_idx_list': result[1],
+        'images': result[2]
+    }
+    return jsonify(response)
 
 
-@app.route('/yolo')
-def yolo(images):
-    return run_yolov5(images)
+@app.route('/yolo', methods=['POST'])
+def yolo():
+    payload = request.get_json()
+    result = run_yolov5(payload['image_list'])
+    return jsonify(result)
 
 
 if __name__ == "__main__":
--- face_recognition/face_recognition.py
+++ face_recognition/face_recognition.py
@@ -15,7 +15,7 @@ import torchvision.datasets as datasets
 import torch.nn.functional as F
 import torchvision.transforms as transforms
 
-from backbone import Backbone
+from .backbone import Backbone
 
 #----------------------------------------------------------
 #                        functions 
